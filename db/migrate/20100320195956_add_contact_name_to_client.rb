847ba1845bdccb47394c4cd728c87bfa0f238eeb
Octavio Alvarez <alvarezp@alvarezp.ods.org>
2010-03-20 13:04:08 -0700

    Edit: actually code the migration.

--- db/migrate/20100320195956_add_contact_name_to_client.rb
+++ db/migrate/20100320195956_add_contact_name_to_client.rb
@@ -1,7 +1,9 @@
 class AddContactNameToClient < ActiveRecord::Migration
   def self.up
+    add_column :clients, :contact_name, :string
   end
 
   def self.down
+    remove_column :clients, :contact_name
   end
 end
